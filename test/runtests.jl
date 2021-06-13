671a0c4e2a229bf256d091eab0bf466ac26213f4
Tim Holy <tim.holy@gmail.com>
2021-06-13 05:33:46 -0500

    Switch to log-likelihood in training

--- test/runtests.jl
+++ test/runtests.jl
@@ -114,7 +114,7 @@ end
         σsels = σyields = σrs = σts = [0.01, Inf]
         # Case 1: rank is meaningful, nothing else is
         applicants = vec([NormalizedApplicant(; program=prog, rank=r, offerdate=od, decidedate=dd, accept=r>3, program_history) for prog in ("CB", "NS"), r in 1:6, od in offerdates, dd in decidedates])
-        np = net_probability(σsels, σyields, σrs, σts; applicants, program_history, rmatch_floor=0.25)
+        np = net_loglike(σsels, σyields, σrs, σts; applicants, program_history, minfrac=0.25)
         @test all(iszero, np[:,:,2,:])
         @test isinf(np[1,1,1,1])
         idx = argmax(np)
@@ -122,7 +122,7 @@ end
         @test idx[4] == 2
         # Case 2: offer date is meaningful, nothing else is
         applicants = vec([NormalizedApplicant(; program=prog, rank=r, offerdate=od, decidedate=dd, accept=month(od)==1, program_history) for prog in ("CB", "NS"), r in 1:6, od in offerdates, dd in decidedates])
-        np = net_probability(σsels, σyields, σrs, σts; applicants, program_history, rmatch_floor=0.25)
+        np = net_loglike(σsels, σyields, σrs, σts; applicants, program_history, minfrac=0.25)
         @test isinf(np[1,1,1,1])
         idx = argmax(np)
         @test idx[3] == 2
@@ -130,7 +130,7 @@ end
         # Case 3: program yield timing and rank are meaningful, nothing else is
         progmonth = Dict("CB" => 3, "NS" => 4)
         applicants = vec([NormalizedApplicant(; program=prog, rank=r, offerdate=od, decidedate=dd, accept=r>=progmonth[prog] && month(dd)==progmonth[prog], program_history) for prog in ("CB", "NS"), r in 1:6, od in offerdates, dd in decidedates])
-        np = net_probability(σsels, σyields, σrs, σts; applicants, program_history, rmatch_floor=0)
+        np = net_loglike(σsels, σyields, σrs, σts; applicants, program_history, minfrac=0)
         @test np[1,:,:,:] ≈ np[2,:,:,:]
         @test np[:,:,:,1] ≈ np[:,:,:,2]
         @test !(np[:,1,:,:] ≈ np[:,2,:,:])
@@ -143,7 +143,7 @@ end
             program_history[ProgramKey(prog, yr)] = ProgramData(slots=10, napplicants=progapps[prog], firstofferdate=Date("$yr-01-13"), lastdecisiondate=Date("$yr-04-15"))
         end
         applicants = vec([NormalizedApplicant(; program=prog, rank=r*progapps[prog]÷100, offerdate=od, decidedate=dd, accept=prog=="CB" ? isodd(r) : iseven(r), program_history) for prog in ("CB", "NS"), r in 1:6, od in offerdates, dd in decidedates])
-        np = net_probability(σsels, σyields, σrs, σts; applicants, program_history, rmatch_floor=0)
+        np = net_loglike(σsels, σyields, σrs, σts; applicants, program_history, minfrac=0)
         @test np[:,1,:,:] ≈ np[:,2,:,:]
         @test np[:,:,:,1] ≈ np[:,:,:,2]
         @test !(np[1,:,:,:] ≈ np[2,:,:,:])
